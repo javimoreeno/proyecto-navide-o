bf25bff6df82e26e6fceef40e61a35c96c506fae
javimoreeno <115158222+javimoreeno@users.noreply.github.com>
2022-12-19 14:16:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-
- function draw(){
-    clear();
-    console.log(frameCount);
-    var x = 50;
-    ellipse(30, frameCount%700, 25,25);
-}
 function setup(){
     createCanvas(1250,700);
     background(255,0 , 0);
+}
+function draw(){
+    clear();
+    console.log(frameCount);
+    var y = frameCount%700;
+    for(var i=0; i <= 10; i++){
+        ellipse(170*i + 20, y, 25,25);
+    }
 }
